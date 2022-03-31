049ea33e8dcf79516a364a3c47445ef67a10dbfc
BrandenBoucher <bgbouche@asu.edu>
2022-03-31 13:20:05 -0700

    Ensuring everything is checked in for the OLED

--- main/main.cpp
+++ main/main.cpp
@@ -13,6 +13,10 @@
 
 #include "minimp3.h"
 
+#include "ssd1306_i2c.h"
+#include "ssd1306_spi.h"
+#include "font8x8_basic.h"
+
 // SD Card
 sdmmc_host_t host = SDSPI_HOST_DEFAULT();
 sdmmc_card_t *card;
@@ -40,6 +44,8 @@ bool back_pressed = false;
 int64_t play_button_debounce_start_time = 0;
 int64_t next_button_debounce_start_time = 0;
 int64_t back_button_debounce_start_time = 0;
+
+SSD1306_t dev;
 
 extern "C"
 {
@@ -136,6 +142,158 @@ static void gpio_task_example(void *arg)
 // 	/* The timer has been created but is not running yet */
 // }
 
+void init_display()
+{
+
+
+
+	#if CONFIG_I2C_INTERFACE
+		ESP_LOGI("main", "INTERFACE is i2c");
+		ESP_LOGI("main", "CONFIG_SDA_GPIO=%d",CONFIG_SDA_GPIO);
+		ESP_LOGI("main", "CONFIG_SCL_GPIO=%d",CONFIG_SCL_GPIO);
+		ESP_LOGI("main", "CONFIG_RESET_GPIO=%d",CONFIG_RESET_GPIO);
+		i2c_master_init(&dev, (gpio_num_t)CONFIG_SDA_GPIO, (gpio_num_t)CONFIG_SCL_GPIO, (gpio_num_t)CONFIG_RESET_GPIO);
+	#endif // CONFIG_I2C_INTERFACE
+
+	#if CONFIG_SPI_INTERFACE
+		ESP_LOGI("main", "INTERFACE is SPI");
+		ESP_LOGI("main", "CONFIG_MOSI_GPIO=%d",CONFIG_MOSI_GPIO);
+		ESP_LOGI("main", "CONFIG_SCLK_GPIO=%d",CONFIG_SCLK_GPIO);
+		ESP_LOGI("main", "CONFIG_CS_GPIO=%d",CONFIG_CS_GPIO);
+		ESP_LOGI("main", "CONFIG_DC_GPIO=%d",CONFIG_DC_GPIO);
+		ESP_LOGI("main", "CONFIG_RESET_GPIO=%d",CONFIG_RESET_GPIO);
+		spi_master_init(&dev, (gpio_num_t)CONFIG_MOSI_GPIO, (gpio_num_t)CONFIG_SCLK_GPIO, (gpio_num_t)CONFIG_CS_GPIO, (gpio_num_t)CONFIG_DC_GPIO, (gpio_num_t)CONFIG_RESET_GPIO);
+	#endif // CONFIG_SPI_INTERFACE
+
+	#if CONFIG_FLIP
+		dev._flip = true;
+		ESP_LOGW("main", "Flip upside down");
+	#endif
+
+
+	ESP_LOGI("main", "Panel is 128x32");
+	ssd1306_init(&dev, 128, 32);
+	ssd1306_clear_screen(&dev, false);
+	ssd1306_contrast(&dev, 0xff);
+}
+
+void run_display_example_task(void *param)
+{
+	while (1)
+	{
+		int center, top, bottom;
+		char lineChar[20];
+		top = 1;
+		center = 1;
+		bottom = 4;
+		ssd1306_display_text(&dev, 0, "SSD1306 128x32", 14, false);
+		ssd1306_display_text(&dev, 1, "Hello World!!", 13, false);
+		ssd1306_clear_line(&dev, 2, true);
+		ssd1306_clear_line(&dev, 3, true);
+		ssd1306_display_text(&dev, 2, "SSD1306 128x32", 14, true);
+		ssd1306_display_text(&dev, 3, "Hello World!!", 13, true);
+
+		vTaskDelay(3000 / portTICK_PERIOD_MS);
+		
+		// Display Count Down
+		ESP_LOGI("main", "Display Count Down");
+		uint8_t image[24];
+		memset(image, 0, sizeof(image));
+		ssd1306_display_image(&dev, top, (6*8-1), image, sizeof(image));
+		ssd1306_display_image(&dev, top+1, (6*8-1), image, sizeof(image));
+		ssd1306_display_image(&dev, top+2, (6*8-1), image, sizeof(image));
+		for(int font=0x39;font>0x30;font--) {
+			memset(image, 0, sizeof(image));
+			ssd1306_display_image(&dev, top+1, (7*8-1), image, 8);
+			memcpy(image, font8x8_basic_tr[font], 8);
+			if (dev._flip) ssd1306_flip(image, 8);
+			ssd1306_display_image(&dev, top+1, (7*8-1), image, 8);
+			vTaskDelay(1000 / portTICK_PERIOD_MS);
+		}
+		
+		// Scroll Up
+		ESP_LOGI("main", "Scroll Up");
+		ssd1306_clear_screen(&dev, false);
+		ssd1306_contrast(&dev, 0xff);
+		ssd1306_display_text(&dev, 0, "---Scroll  UP---", 16, true);
+		//ssd1306_software_scroll(&dev, 7, 1);
+		ssd1306_software_scroll(&dev, (dev._pages - 1), 1);
+		for (int line=0;line<bottom+10;line++) {
+			lineChar[0] = 0x01;
+			sprintf(&lineChar[1], " Line %02d", line);
+			ssd1306_scroll_text(&dev, lineChar, strlen(lineChar), false);
+			vTaskDelay(500 / portTICK_PERIOD_MS);
+		}
+		vTaskDelay(3000 / portTICK_PERIOD_MS);
+		
+		// Scroll Down
+		ESP_LOGI("main", "Scroll Down");
+		ssd1306_clear_screen(&dev, false);
+		ssd1306_contrast(&dev, 0xff);
+		ssd1306_display_text(&dev, 0, "--Scroll  DOWN--", 16, true);
+		//ssd1306_software_scroll(&dev, 1, 7);
+		ssd1306_software_scroll(&dev, 1, (dev._pages - 1) );
+		for (int line=0;line<bottom+10;line++) {
+			lineChar[0] = 0x02;
+			sprintf(&lineChar[1], " Line %02d", line);
+			ssd1306_scroll_text(&dev, lineChar, strlen(lineChar), false);
+			vTaskDelay(500 / portTICK_PERIOD_MS);
+		}
+		vTaskDelay(3000 / portTICK_PERIOD_MS);
+
+		// Page Down
+		ESP_LOGI("main", "Page Down");
+		ssd1306_clear_screen(&dev, false);
+		ssd1306_contrast(&dev, 0xff);
+		ssd1306_display_text(&dev, 0, "---Page	DOWN---", 16, true);
+		ssd1306_software_scroll(&dev, 1, (dev._pages-1) );
+		for (int line=0;line<bottom+10;line++) {
+			//if ( (line % 7) == 0) ssd1306_scroll_clear(&dev);
+			if ( (line % (dev._pages-1)) == 0) ssd1306_scroll_clear(&dev);
+			lineChar[0] = 0x02;
+			sprintf(&lineChar[1], " Line %02d", line);
+			ssd1306_scroll_text(&dev, lineChar, strlen(lineChar), false);
+			vTaskDelay(500 / portTICK_PERIOD_MS);
+		}
+		vTaskDelay(3000 / portTICK_PERIOD_MS);
+
+		// Horizontal Scroll
+		ESP_LOGI("main", "Horizontal Scroll");
+		ssd1306_clear_screen(&dev, false);
+		ssd1306_contrast(&dev, 0xff);
+		ssd1306_display_text(&dev, center, "Horizontal", 10, false);
+		ssd1306_hardware_scroll(&dev, SCROLL_RIGHT);
+		vTaskDelay(5000 / portTICK_PERIOD_MS);
+		ssd1306_hardware_scroll(&dev, SCROLL_LEFT);
+		vTaskDelay(5000 / portTICK_PERIOD_MS);
+		ssd1306_hardware_scroll(&dev, SCROLL_STOP);
+		
+		// Vertical Scroll
+		ESP_LOGI("main", "Vertical Scroll");
+		ssd1306_clear_screen(&dev, false);
+		ssd1306_contrast(&dev, 0xff);
+		ssd1306_display_text(&dev, center, "Vertical", 8, false);
+		ssd1306_hardware_scroll(&dev, SCROLL_DOWN);
+		vTaskDelay(5000 / portTICK_PERIOD_MS);
+		ssd1306_hardware_scroll(&dev, SCROLL_UP);
+		vTaskDelay(5000 / portTICK_PERIOD_MS);
+		ssd1306_hardware_scroll(&dev, SCROLL_STOP);
+		
+		// Invert
+		ESP_LOGI("main", "Invert");
+		ssd1306_clear_screen(&dev, true);
+		ssd1306_contrast(&dev, 0xff);
+		ssd1306_display_text(&dev, center, "  Good Bye!!", 12, true);
+		vTaskDelay(5000 / portTICK_PERIOD_MS);
+
+
+		// Fade Out
+		ESP_LOGI("main", "Fade Out");
+		ssd1306_fadeout(&dev);
+		
+	}
+}
+
 void init_inputs()
 {
 	gpio_config_t io_conf = {
@@ -206,7 +364,9 @@ bool mount_sd_card(void)
 		.quadhd_io_num = -1,
 		.max_transfer_sz = 4000,
 	};
-	ret = spi_bus_initialize(static_cast<spi_host_device_t>(host.slot), &bus_cfg, SPI_DMA_CHAN);
+	
+	//ret = spi_bus_initialize(static_cast<spi_host_device_t>(host.slot), &bus_cfg, SPI_DMA_CHAN);
+	ret = spi_bus_initialize(SPI2_HOST, &bus_cfg, SPI_DMA_CHAN);
 	if (ret != ESP_OK)
 	{
 		ESP_LOGE("main", "Failed to initialize bus.");
@@ -253,13 +413,59 @@ void init_audio_out()
 	// gpio_set_pull_mode(START_BUTTON, GPIO_PULLUP_ONLY);
 }
 
+void scroll_text(char * text, int str_len, int max_len, int start_pos, char * buffer)
+{
+	
+
+	for (int i = 0; i < max_len; i++)
+	{
+		if (i + start_pos > str_len - 1)
+			buffer[i] = ' ';
+		else
+			buffer[i] = text[i + start_pos];
+	}
+
+}
+
+int scroll_pos = 0;
+int file_name_len= 0;
+
 void update_display()
 {
 	int totalSeconds = mp3_run_time / 1000000;
 	int minutes = totalSeconds / 60;
 	int seconds = totalSeconds % 60;
+	
+	char time_disp[20];
 
-	printf("%s, run time: %d:%02d\n", file_name, minutes, seconds);
+	char buff[MAX_CHARS];
+
+	// printf("%s, run time: %d:%02d\n", file_name, minutes, seconds);
+	time_disp[0] = NULL;
+	sprintf(time_disp, "%02d:%02d", minutes, seconds);	
+
+
+	printf("%s, run time: %s\n", file_name, time_disp);
+
+	// ssd1306_clear_line(&dev, 0, true);
+	// ssd1306_clear_line(&dev, 1, true);
+
+
+	scroll_pos++;
+
+	if (scroll_pos > file_name_len - MAX_CHARS)	
+		scroll_pos = 0;
+
+	scroll_text(file_name, file_name_len, MAX_CHARS, scroll_pos, buff);
+
+	ssd1306_display_text(&dev, 0, buff, strlen(buff), false);
+	ssd1306_display_text(&dev, 1, time_disp, strlen(time_disp), false);
+	if (pause_toggle)
+		ssd1306_display_text(&dev, 2, "paused ", 7, false);
+	else	
+		ssd1306_display_text(&dev, 2, "playing", 7, false);
+	
+
 }
 
 void play_mp3(FILE *fp)
@@ -380,6 +586,7 @@ void play_mp3(FILE *fp)
 				output->write(pcm, samples);
 				// ESP_LOGI("main", "output written\n");
 				//  keep track of how many samples we've decoded
+				printf("%d\n", samples);
 				decoded += samples;
 			}
 			// ESP_LOGI("main", "decoded %d samples\n", decoded);
@@ -420,6 +627,8 @@ void play_task(void *param)
 		strcpy(file_name, get_current_path());
 		strcat(file_name, "/");
 		strcat(file_name, get_current_file()->d_name);
+		file_name_len = strlen(file_name);
+		scroll_pos = 0;
 		// this assumes that you have uploaded the mp3 file to the SPIFFS
 		ESP_LOGI("main", "Opening file %s", file_name);
 		FILE *fp = fopen(file_name, "r");
@@ -432,7 +641,7 @@ void play_task(void *param)
 		// fseek(fp, 0L, SEEK_END);
 		// file_sz = ftell(fp);
 		// rewind(fp);
-
+	
 		play_mp3(fp);
 		fclose(fp);
 		ESP_LOGI("main", "Finished\n");
@@ -465,11 +674,13 @@ void app_main()
 {
 	init_inputs();
 	// init_timer();
-
+init_display();
 	// ESP_ERROR_CHECK(esp_timer_start_periodic(periodic_timer_handle, 1000));
 
 	if (mount_sd_card())
 	{
 		xTaskCreatePinnedToCore(play_task, "task", 32768, NULL, 1, NULL, 1);
 	}
+
+//xTaskCreatePinnedToCore(run_display_example_task, "display_task", 32768, NULL, 1, NULL, 0);
 }
