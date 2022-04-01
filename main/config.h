9b1d2523164c0169151bee07927b44f0c5568f58
BrandenBoucher <bgbouche@asu.edu>
2022-03-31 17:38:17 -0700

    Integrated BT code. Audio not connected yet.

--- main/config.h
+++ main/config.h
@@ -1,4 +1,5 @@
 #include <freertos/FreeRTOS.h>
+#include "freertos/timers.h"
 #include <driver/i2s.h>
 
 
@@ -37,3 +38,40 @@
 #define RESTART_CURRENT_US 2000000
 // i2s speaker pins definition
 extern i2s_pin_config_t i2s_speaker_pins;
+
+
+#define BT_AV_TAG				"BT_AV"
+#define BT_RC_CT_TAG			"RCCT"
+
+// number of retries when connecting
+#define BT_CONNECT_RETRY 2
+
+// AVRCP used transaction label
+#define APP_RC_CT_TL_GET_CAPS			 (0)
+#define APP_RC_CT_TL_RN_VOLUME_CHANGE	 (1)
+
+/* event for handler "bt_av_hdl_stack_up */
+enum {
+	BT_APP_EVT_STACK_UP = 0,
+};
+
+/* A2DP global state */
+enum {
+	APP_AV_STATE_IDLE,
+	APP_AV_STATE_DISCOVERING,
+	APP_AV_STATE_DISCOVERED,
+	APP_AV_STATE_UNCONNECTED,
+	APP_AV_STATE_CONNECTING,
+	APP_AV_STATE_CONNECTED,
+	APP_AV_STATE_DISCONNECTING,
+};
+
+/* sub states of APP_AV_STATE_CONNECTED */
+enum {
+	APP_AV_MEDIA_STATE_IDLE,
+	APP_AV_MEDIA_STATE_STARTING,
+	APP_AV_MEDIA_STATE_STARTED,
+	APP_AV_MEDIA_STATE_STOPPING,
+};
+
+#define BT_APP_HEART_BEAT_EVT				 (0xff00)
